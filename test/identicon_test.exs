166ab7eb1b164aba188db86adfb374d366bac858
Henrique Almeida <henrique_af1@msn.com>
2018-07-27 10:15:12 -0300

    Added some tests

--- test/identicon_test.exs
+++ test/identicon_test.exs
@@ -2,7 +2,14 @@ defmodule IdenticonTest do
   use ExUnit.Case
   doctest Identicon
 
-  test "greets the world" do
-    assert Identicon.hello() == :world
+  test "It should return :ok" do
+    status = Identicon.main("test")
+    assert status == :ok
+    File.rm("test.png")
+  end
+
+  test "It should generate an %Identicon.Image struct with the string converted to bin list" do
+    test_string = "test"
+    assert Identicon.hash_input(test_string) == %Identicon.Image{hex: :binary.bin_to_list(:crypto.hash(:md5, test_string))}
   end
 end
